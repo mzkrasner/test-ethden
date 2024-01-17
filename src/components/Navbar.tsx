1ec9dbd6b311364067e49c2d56f600f0d2799616
Mark Krasner <markkrasner@Marks-MacBook.local>
2024-01-17 18:23:53 -0500

    editing home link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Nav: React.FC = () => {
    
 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="#" className="flex items-center">
+  <a href="/" className="flex items-center">
       <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="" />
       <span className="self-center text-1xl font-semibold whitespace-nowrap dark:text-white">EthDenver24 Scavenger Hunt</span>
   </a>
